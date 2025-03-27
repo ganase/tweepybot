be3ba0683d4b49bb82e0e778e76386645891f736
nagnag <m.nagase@thk.co.jp>
2025-03-27 11:41:05 +0900

    Add files via upload

--- tweepybot_scraping_tuge.py
+++ tweepybot_scraping_tuge.py
@@ -25,7 +25,7 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")  # GUIを表示しないモード
-
+chrome_options.binary_location = '/usr/bin/chromium-browser'
 driver = webdriver.Chrome(options=chrome_options)
 
 # Tableau User Groupsのイベントページを開く
